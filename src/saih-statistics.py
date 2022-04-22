cb2a9fbd55148e08cdb89b174f0d457ac9ccfb6a
jke94 <jcarracedo.dev@gmail.com>
2022-04-22 21:30:07 +0200

    Add place and save the data in csv files.

--- src/saih-statistics.py
+++ src/saih-statistics.py
@@ -1,15 +1,18 @@
 from operator import le
 import pandas as pd
+import os
 # import matplotlib
 
 nombres = [
+        'Velilla de La Valduerna',
         'Morla de la Valderia',
         'Nogarejas',
         'Castrocalbon',
         'Corporales',
         'Truchillas']
 
-temperatura = [      
+temperatura = [
+        'http://www.saihduero.es/risr/EA520/historico/xATRUFURfBjM1EUR', #Velilla de La Valduerna      
         'http://www.saihduero.es/risr/EA089/historico/xATRUFURflDOwEUR', # Morla de la Valderia
         'http://www.saihduero.es/risr/PL283/historico/xATRUFURfNDOywEU', # Nogarejas
         'http://www.saihduero.es/risr/PL284/historico/xATRUFURfRDOywEU', # Castrocalbon
@@ -18,6 +21,7 @@ temperatura = [
 ]
 
 publiometria = [
+        'http://www.saihduero.es/risr/EA520/historico/xADTQNURfBjM1EUR', #Velilla de La Valduerna
         'http://www.saihduero.es/risr/EA089/historico/xADTQNURflDOwEUR', # Morla de la Valderia
         'http://www.saihduero.es/risr/PL283/historico/xADTQNURfNDOywEU', # Nogarejas
         'http://www.saihduero.es/risr/PL284/historico/xADTQNURfRDOywEU', # Castrocalbon
@@ -25,10 +29,17 @@ publiometria = [
         'http://www.saihduero.es/risr/PL281/historico/xADTQNURfFDOywEU'  # Truchillas
 ]
 
-for index in range(len(temperatura)):
+os.makedirs('../data/rain', exist_ok=True)
+
+for index in range(len(publiometria)):
         print("Data from: ", nombres[index])
-        dataframes = pd.read_html(temperatura[index],
+        dataframes = pd.read_html(publiometria[index],
                 decimal=',',thousands='.') # Three dataframes.
+        
+        fileName = '../data/rain/' + nombres[index].replace(' ','-') + '.csv'
+
+        dataframes[0].to_csv(fileName, index=False)
+
         print('Dataframe len():', len(dataframes), 'type(dataframses):', type(dataframes))
         for i in range(len(dataframes)):
                 print(dataframes[i])
