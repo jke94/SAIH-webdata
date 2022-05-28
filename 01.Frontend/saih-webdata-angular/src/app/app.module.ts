c56a988d8a07eecb389fb2f7e8230f4f79f26510
jke94 <jcarracedo.dev@gmail.com>
2022-05-28 13:50:04 +0200

    Add main component.

--- 01.Frontend/saih-webdata-angular/src/app/app.module.ts
+++ 01.Frontend/saih-webdata-angular/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { GaugingStationComponent } from './gauging-station/gauging-station.component';
+import { GaugingStationComponent } from './components/gauging-station/gauging-station.component';
 import { PluviometricStationsComponent } from './components/pluviometric-stations/pluviometric-stations.component';
 import { ReservoirStationComponent } from './components/reservoir-station/reservoir-station.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { ToolbarComponent } from './components/toolbar/toolbar.component';
     GaugingStationComponent,
     PluviometricStationsComponent,
     ReservoirStationComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
