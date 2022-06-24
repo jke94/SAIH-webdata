252343f17c9caabedd81cf05381971dabffb38e6
jke94 <jcarracedo.dev@gmail.com>
2022-06-24 13:01:28 +0200

    Add GaugingStationComponent.

--- 01.Frontend/saih-webdata-angular/src/app/components/gauging-station/gauging-station.component.ts
+++ 01.Frontend/saih-webdata-angular/src/app/components/gauging-station/gauging-station.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { GaugingStationService } from 'src/app/services/gauging-station/gauging-station.service';
+import { GaugingStation } from 'src/app/models/gauging-station';
+
 
 @Component({
   selector: 'app-gauging-station',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GaugingStationComponent implements OnInit {
 
-  constructor() { }
+  gaugingStations: GaugingStation[];
+  displayedColumns = ['station', 'river', 'lat', 'lng'];
 
-  ngOnInit(): void {
+  constructor(private gaugingStationService: GaugingStationService) {
+    this.gaugingStations = []
   }
 
+  ngOnInit() {
+    this.gaugingStationService.getJSON().subscribe(data => {
+      this.gaugingStations = data as GaugingStation[];
+      console.log(this.gaugingStations)
+    });
+  }
 }
