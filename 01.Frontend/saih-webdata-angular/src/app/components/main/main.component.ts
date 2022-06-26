3d6a44251ee844bfc962d077f5bec4b4cde2f122
jke94 <jcarracedo.dev@gmail.com>
2022-06-26 11:15:23 +0200

    Obsolete info.

--- 01.Frontend/saih-webdata-angular/src/app/components/main/main.component.ts
+++ 01.Frontend/saih-webdata-angular/src/app/components/main/main.component.ts
@@ -1,24 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
-export interface Tile {
-  color: string;
-  cols: number;
-  rows: number;
-  text: string;
-}
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
-
-  tiles: Tile[] = [
-    {text: 'Saih web data URL',  cols: 4, rows: 1, color: '#009efb'},
-    {text: 'Tabs',     cols: 4, rows: 6, color: 'lightblue'},
-    {text: 'Footer',  cols: 4, rows: 1, color: '#009efb'}
-    
-  ];
 
   constructor() { }
 
